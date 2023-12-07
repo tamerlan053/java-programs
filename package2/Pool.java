8d6405af0d91d6618859c5543358aa1b82a8f7ac
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-07 17:28:21 +0100

    Update Pool.java

--- package2/Pool.java
+++ package2/Pool.java
@@ -1,1 +1,32 @@
+package packages.package2;
+
+public class SwimmingPool {
+    private double length;
+    private double width;
+    private double depth;
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public int getVolume() {
+        return (int) (length * width * depth * 1000);
+    }
+
+    public int getWaterVolume() {
+        return (int) (getVolume() * 0.98);
+    }
+
+    public int getDisinfectantVolume() {
+        return (int) (getVolume() * 0.02);
+    }
+}
 
