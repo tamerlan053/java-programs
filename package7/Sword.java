988618854793476de05452a2756f62a4c339acc7
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-13 23:10:27 +0100

    Create Sword.java

---
+++ package7/Sword.java
@@ -0,0 +1,22 @@
+package packages.package7;
+
+public class Sword {
+    private String material;
+    private int durability;
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+}
