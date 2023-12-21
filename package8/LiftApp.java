3d277ead4271729867ddd54f334109b9bf2e11ce
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-12-21 23:08:13 +0100

    Create LiftApp.java

---
+++ package8/LiftApp.java
@@ -0,0 +1,43 @@
+package packages.package8;
+
+import java.util.Scanner;
+
+public class LiftApp {
+    public static void main(String[] args) {
+        Elevator elevator = new Elevator();
+        elevator.setMaximumCapacity(5);
+        elevator.setNumberOfFloors(11);
+        System.out.println("""
+                Make your choice:\s
+                1. Go to floor ...
+                2. Enter the elevator ...
+                3. Exit the elevator...
+                4. Quit""");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        while (choice != 4) {
+            if (choice == 1) {
+                System.out.print("Choose a floor: \n");
+                int floor = scanner.nextInt();
+                elevator.goTo(floor);
+            } else if (choice == 2) {
+                System.out.print("Number of people: \n");
+                int numberOfPeople = scanner.nextInt();
+                elevator.enter(numberOfPeople);
+            } else if (choice == 3) {
+                System.out.print("Number of people: \n");
+                int numberOfPeople = scanner.nextInt();
+                elevator.exit(numberOfPeople);
+            } else {
+                break;
+            }
+            System.out.println("""
+                Make your choice:\s
+                1. Go to floor ...
+                2. Enter the elevator ...
+                3. Exit the elevator...
+                4. Quit""");
+            choice = scanner.nextInt();
+        }
+    }
+}
