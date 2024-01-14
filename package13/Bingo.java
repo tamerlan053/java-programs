3fb85a4345fd866bfd8c600e1bf6e186ac3e34ca
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-14 23:23:21 +0100

    Create Bingo.java

---
+++ package13/Bingo.java
@@ -0,0 +1,12 @@
+package packages.package13;
+
+import java.util.ArrayList;
+
+public class Bingo {
+    private String name;
+    private ArrayList<Integer> numbers = new ArrayList<>();
+
+    public Bingo(String name) {
+        this.name = name;
+    }
+}
