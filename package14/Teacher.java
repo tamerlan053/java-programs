969687d5ec169554ed2e4d579758c721d3680c5c
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-16 17:28:05 +0100

    Create Teacher.java

---
+++ package14/Teacher.java
@@ -0,0 +1,9 @@
+package exercises.ex3;
+
+public class Teacher {
+    private static final int MAX = 5;
+    private String lastName;
+    private String firstName;
+    private int employmentPercentage;
+    private Subject[] subjects = new Subject[MAX];
+}
