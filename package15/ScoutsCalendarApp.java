c2b74a7feaf49a27e18dff858f17a51a644949fa
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-22 20:01:04 +0100

    Update ScoutsCalendarApp.java

--- package15/ScoutsCalendarApp.java
+++ package15/ScoutsCalendarApp.java
@@ -4,3 +4,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+public class ScoutsCalendarApp {
+    public static void main(String[] args) {
+    }
+}
