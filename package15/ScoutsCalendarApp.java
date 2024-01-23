13abf4e80ef5535a5b39e4c50de7ba8843edbf12
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-23 23:15:05 +0100

    Update ScoutsCalendarApp.java

--- package15/ScoutsCalendarApp.java
+++ package15/ScoutsCalendarApp.java
@@ -7,5 +7,7 @@ import java.util.Scanner;
 
 public class ScoutsCalendarApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM yyyy");
     }
 }
