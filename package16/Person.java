c0fbd96c449c1666c4ea8491136cdf8d680bf4f3
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-25 20:42:11 +0100

    Update Person.java

--- package16/Person.java
+++ package16/Person.java
@@ -3,4 +3,9 @@ package packages.package16;
 public class Person {
     private String firstName;
     private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
