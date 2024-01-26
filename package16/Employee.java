1c27376cbfc57ff835562b8026e35aade8f17b9b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-26 21:33:40 +0100

    Update Employee.java

--- package16/Employee.java
+++ package16/Employee.java
@@ -3,4 +3,6 @@ package packages.package16;
 public class Employee extends Person {
     private String position;
     private double salary;
+    private static final double MINIMUM_SALARY = 1000;
+    private static int count;
 }
