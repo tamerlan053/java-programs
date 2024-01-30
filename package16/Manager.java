4b726b3617b5d38022d0d1cde8f179abb4a203f9
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-01-31 00:06:58 +0100

    Update Manager.java

--- package16/Manager.java
+++ package16/Manager.java
@@ -3,4 +3,8 @@ package packages.package16;
 public class Manager extends Employee {
     private double bonus;
     private static int count;
+
+    public Manager(String lastName, String firstName, String position, double salary) {
+        this(lastName, firstName, position, salary, 50);
+    }
 }
