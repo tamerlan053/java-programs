33090e915b4bf95d9183f23ccdbd3c6bf0891aae
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-06-03 23:21:18 +0200

    Update Calculator.java
    basic properties added

--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculatorGUI extends JFrame implements ActionListener {
-  
+    private JTextField textField;
+    private JButton[] numberButtons;
+    private JButton[] functionButtons;
+    private JButton addButton, subButton, mulButton, divButton, equButton, decButton, clrButton, delButton;
+    private JPanel panel;
 }
