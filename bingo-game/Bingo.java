a7af3d3313dad00b5b14a1ad0a83a5a97ed150b0
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-01 23:10:42 +0200

    Update Bingo.java
    bingo class created

--- bingo-game/Bingo.java
+++ bingo-game/Bingo.java
@@ -1,1 +1,26 @@
+package bingo;
 
+import java.util.ArrayList;
+
+public class Bingo {
+    private String name;
+    private ArrayList<Integer> numbers = new ArrayList<>();
+
+    public Bingo(String name) {
+        this.name = name;
+    }
+
+    public void addNumber(int number) {
+        numbers.add(number);
+    }
+
+    public boolean hasNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append(" ").append(numbers);
+        return stringBuilder.toString();
+    }
+}
