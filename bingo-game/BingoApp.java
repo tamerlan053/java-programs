ab7375db0ac7cdb5476368963e508959a9aa1d97
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-01 23:13:04 +0200

    Update BingoApp.java
    bingo cards added

--- bingo-game/BingoApp.java
+++ bingo-game/BingoApp.java
@@ -4,5 +4,13 @@ import java.util.Random;
 
 public class BingoApp {
     public static void main(String[] args) {
+        Bingo[] bingoCards = new Bingo[5];
+        Random random = new Random();
+
+        bingoCards[0] = new Bingo("Jason");
+        bingoCards[1] = new Bingo("Matt");
+        bingoCards[2] = new Bingo("Willems");
+        bingoCards[3] = new Bingo("John");
+        bingoCards[4] = new Bingo("Brad");
     }
 }
