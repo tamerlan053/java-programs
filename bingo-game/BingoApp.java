54c2fd24d7d3048f0e48b8b69ccbf0199c55e63e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-02 23:46:06 +0200

    Update BingoApp.java
    for loop added

--- bingo-game/BingoApp.java
+++ bingo-game/BingoApp.java
@@ -12,5 +12,12 @@ public class BingoApp {
         bingoCards[2] = new Bingo("Willems");
         bingoCards[3] = new Bingo("John");
         bingoCards[4] = new Bingo("Brad");
+
+        for (int i = 0; i < bingoCards.length; i++) {
+            for (int j = 0; j < 6; j++) {
+                int randomNumber = random.nextInt(101);
+                bingoCards[i].addNumber(randomNumber);
+            }
+        }
     }
 }
