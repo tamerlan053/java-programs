51ddd4d9977663389f9724e50e1232d9cd0c6817
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-04 23:46:30 +0200

    Update Image.java
    execute function finished

--- redactor/Image.java
+++ redactor/Image.java
@@ -13,6 +13,16 @@ public class Image extends File {
 
     @Override
     public void execute() {
-        
+        System.out.println(getName());
+
+        Random random = new Random();
+
+        for (int i = 0; i < dimensions[0]; i++) {
+            for (int j = 0; j < dimensions[1]; j++) {
+                char randomChar = getContent().charAt(random.nextInt(getContent().length()));
+                System.out.print(randomChar);
+            }
+            System.out.println();
+        }
     }
 }
