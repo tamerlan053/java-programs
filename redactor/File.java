eaf41de7e2951fd3ed0400afdb0f59b77e22c07f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-07 23:32:09 +0200

    Update File.java
    constructors added

--- redactor/File.java
+++ redactor/File.java
@@ -4,4 +4,8 @@ public abstract class File implements Executable {
     private String name;
     private String content;
     private int fileSize;
+
+    public File(String name) {
+        this(name, 1024);
+    }
 }
