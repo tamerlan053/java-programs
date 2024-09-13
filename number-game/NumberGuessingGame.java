d5227a71e495d4edce9ae221bd80b7aeca7fa875
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-13 23:06:46 +0200

    Update NumberGuessingGame.java
    class finished

--- number-game/NumberGuessingGame.java
+++ number-game/NumberGuessingGame.java
@@ -1,1 +1,41 @@
+import java.util.Random;
 
+public class NumberGuessingGame {
+    private int randomNumber;
+    private int numberOfTries;
+    private final int maxTries;
+
+    public NumberGuessingGame(int maxTries) {
+        Random random = new Random();
+        this.randomNumber = random.nextInt(100) + 1;
+        this.numberOfTries = 0;
+        this.maxTries = maxTries;
+    }
+
+    public String makeGuess(int guess) {
+        numberOfTries++;
+        if (guess < randomNumber) {
+            return "Your guess is too low.";
+        } else if (guess > randomNumber) {
+            return "Your guess is too high.";
+        } else {
+            return "correct";
+        }
+    }
+
+    public boolean hasWon(int guess) {
+        return guess == randomNumber;
+    }
+
+    public boolean hasMoreTries() {
+        return numberOfTries < maxTries;
+    }
+
+    public int getNumberOfTries() {
+        return numberOfTries;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+}
